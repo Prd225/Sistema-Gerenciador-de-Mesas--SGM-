4f59dc08293ca8b5dda4abd6b3352ee9868481ea
Ronald Vieira <ronaldvieira614@gmail.com>
2026-09-24 18:03:32 -0400

    test(e2e): configurar playwright com roteiro de 4 passos desktop e mobile

--- apps/web/src/components/master-panel/MasterPanelTrigger.tsx
+++ apps/web/src/components/master-panel/MasterPanelTrigger.tsx
@@ -12,6 +12,7 @@ export default function MasterPanelTrigger() {
     <div className="absolute bottom-[60px] left-1/2 -translate-x-1/2 z-40">
       <button
         onClick={toggleOpen}
+        aria-label="Abrir Painel do Mestre"
         className="group flex flex-col items-center justify-center bg-[#121214]/95 hover:bg-[#202024] border border-[#323238] border-b-0 rounded-t-full w-24 h-8 transition-all shadow-[0_-4px_12px_rgba(0,0,0,0.5)]"
       >
         <ChevronUp className="w-5 h-5 text-[#8257e5] group-hover:-translate-y-1 transition-transform" />
