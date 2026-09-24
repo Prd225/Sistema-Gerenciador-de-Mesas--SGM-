49ae717a8c1aced226720e5b816d270ade3759ea
Ronald Vieira <ronaldvieira614@gmail.com>
2026-09-24 09:43:58 -0400

    chore(tsconfig): configurar alias de path para modulo shared

--- vite.config.ts
+++ vite.config.ts
@@ -161,6 +161,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      '@shared': path.resolve(__dirname, './shared/src'),
     },
   },
   server: {
